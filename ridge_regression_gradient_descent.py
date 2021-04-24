bcb49ff83cec04ad5e11be0fab0b577580eab6bf
snalahi <59950232+snalahi@users.noreply.github.com>
2021-04-24 01:12:47 -0700

    Algorithms and Applications

--- ridge_regression_gradient_descent.py
+++ ridge_regression_gradient_descent.py
@@ -522,4 +522,3 @@ if __name__ == '__main__':
         # TODO: Test model on testing set
         score_mse_grad_descent_test = score_mean_squared_error(ridge_grad_descent, x_test, y_test)
         print('Testing set mean squared error: {:.4f}'.format(score_mse_grad_descent_test))
-
